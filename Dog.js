9a10802df28f21585f287a9b91532b8a3449d296
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-03-27 21:21:26 +0100

    like and cross functionallity

--- Dog.js
+++ Dog.js
@@ -1,1 +1,27 @@
-// Create the Dog class here
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogAvatarSrc() {
+    return this.avatar;
+  }
+
+  getDogHtml() {
+    const { name, age, bio, hasBeenLiked, hasBeenSwiped } = this;
+
+    let stamp = hasBeenLiked ? 
+      '<img class="dog-swipe-like" src="images/badge-like.png" alt="like badge">'
+      : hasBeenSwiped 
+        ? '<img class="dog-swipe-nope" src="images/badge-nope.png" alt=" nope badge">'
+        : '';
+
+    return `
+      <h3 class="dog-name">${name}, ${age}</h3>
+      <p class="dog-quote">${bio}</p>
+      ${stamp}
+    `
+  }
+}
+
+export default Dog
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-// Remember to import the data and Dog class!
+import { dogs } from './data.js'
+import Dog from './Dog.js'
 
-const dogSwipeCross = document.getElementById('dog-swipe-cross')
-const dogSwipeHeart = document.getElementById('dog-swipe-heart')
+const dogSwipeCross = document.getElementById('dog-swipe-cross');
+const dogSwipeHeart = document.getElementById('dog-swipe-heart');
 
-dogSwipeHeart.addEventListener("click", handleHeartClick)
-dogSwipeCross.addEventListener("click", handleCrossClick)
+dogSwipeHeart.addEventListener("click", handleHeartClick);
+dogSwipeCross.addEventListener("click", handleCrossClick);
+
+let isWaiting = false;
 
 function handleHeartClick() {
-  console.log("hit heart")
+  if (!isWaiting) {
+    dog.hasBeenLiked = true;
+    render();
+    isWaiting = true;
+    setTimeout(() => { 
+      if (dogs.length> 0) {
+        dog = getNextDog()
+        render()
+      }
+
+      isWaiting = false;
+    } , 1000);
+  }
 }
 
 function handleCrossClick() {
-  console.log("hit cross")
-
+  if (!isWaiting) {
+    dog.hasBeenSwiped = true;
+    render();
+    isWaiting = true;
+    setTimeout(() => { 
+      if (dogs.length> 0) {
+        dog = getNextDog()
+        render()
+      }
+      isWaiting = false;
+    } , 1000);
+  }
 }
+
+function getNextDog() {
+  const nextDogData = dogs.shift();
+  return nextDogData ? new Dog(nextDogData) : {};
+}
+
+function render() {
+  document.getElementById('dog-swipe-main-img').innerHTML = dog.getDogHtml();
+  document.getElementById('dog-swipe-main-img').style.background = `url(${dog.getDogAvatarSrc()})`
+  document.getElementById('dog-swipe-main-img').style.backgroundSize = 'cover'
+}
+
+let dog = getNextDog();
+render()
