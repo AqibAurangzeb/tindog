3b5031628bc1ace95ff5086a5c375b0ad112a3fc
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-03-27 19:37:00 +0100

    front end complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Remember to import the data and Dog class!
+
+const dogSwipeCross = document.getElementById('dog-swipe-cross')
+const dogSwipeHeart = document.getElementById('dog-swipe-heart')
+
+dogSwipeHeart.addEventListener("click", handleHeartClick)
+dogSwipeCross.addEventListener("click", handleCrossClick)
+
+function handleHeartClick() {
+  console.log("hit heart")
+}
+
+function handleCrossClick() {
+  console.log("hit cross")
+
+}
